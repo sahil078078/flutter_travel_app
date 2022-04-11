4186174003015d866bfb33f760e73e5efde018a3
chirag mevada <chiragmevada204@gmail.com>
2022-04-11 17:38:38 +0530

    1 commit

---
+++ lib/misc/colors.dart
@@ -0,0 +1,11 @@
+import 'dart:ui';
+
+class AppColors {
+  static const Color textColor1 = Color(0xff989acd);
+  static const Color textColor2 = Color(0xff878593);
+  static const Color bigTextColor = Color(0xff2e2e31);
+  static const Color mainColor = Color(0xff989acd);
+  static const Color starColor = Color(0xffe7bb4e);
+  static const mainTextColor = Color(0xffababad);
+  static const buttonBackground = Color(0xff854165);
+}
---
+++ lib/pages/welcome_page.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_travel_app/Widgets/app_large_text.dart';
+import 'package:flutter_travel_app/Widgets/app_text.dart';
+import 'package:flutter_travel_app/misc/colors.dart';
+
+class WelcomePage extends StatefulWidget {
+  const WelcomePage({Key? key}) : super(key: key);
+
+  @override
+  State<WelcomePage> createState() => _WelcomePageState();
+}
+
+class _WelcomePageState extends State<WelcomePage> {
+  List images = [
+    'welcome-one.png',
+    'welcome-two.png',
+    'welcome-three.png',
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: PageView.builder(
+        scrollDirection: Axis.vertical,
+        itemCount: images.length,
+        itemBuilder: (BuildContext context, int index) {
+          return Container(
+            width: double.maxFinite,
+            height: double.maxFinite,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage(
+                  "assets/images/" + images[index],
+                ),
+                fit: BoxFit.cover,
+              ),
+            ),
+            child: Container(
+              margin: const EdgeInsets.only(top: 150, left: 20, right: 20),
+              child: Row(
+                children: <Widget>[
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      AppLargeText(text: 'Trips'),
+                      AppText(text: 'Mountain', size: 30),
+                      Container(
+                        width: 250,
+                        child: AppText(
+                          text:
+                              'Mountain hiles give you an incredible sense of freedom along with endurance test',
+                          color: AppColors.textColor2,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
